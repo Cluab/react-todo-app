385223ab345fd5aec6ec487242a21b4da05502b5
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-12-05 12:38:23 +0200

    add react app/ components & todolis checkbox/delete features

---
+++ src/components/TodoItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class TodoItem extends React.Component {
+  render() {
+    return (
+      <li>
+        <input
+          type="checkbox"
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+          checked={this.props.todo.completed}
+        />
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+        {this.props.todo.title}
+      </li>
+    );
+  }
+}
+
+export default TodoItem;
