6e880224539d91681122ac3ee1da15f84ca7aa7e
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-12-05 15:05:52 +0200

    add styling

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -22,15 +22,16 @@ class InputTodo extends Component {
   };
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} className="form-container">
         <input
           type="text"
+          className="input-text"
+          placeholder="Add todo..."
+          value={this.state.title}
           name="title"
           onChange={this.onChange}
-          placeholder="Add Todo..."
-          value={this.state.title}
         />
-        <button>Submit</button>
+        <button className="input-submit">Submit</button>
       </form>
     );
   }
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TodosList from './TodosList';
 import Header from './Header';
 import InputTodo from './InputTodo';
+// import '../index.css';
 import { v4 as uuidv4 } from 'uuid';
 
 class TodoContainer extends React.Component {
@@ -58,14 +59,16 @@ class TodoContainer extends React.Component {
   };
   render() {
     return (
-      <div>
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodosList
-          todos={this.state.todos}
-          handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
-        />
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
