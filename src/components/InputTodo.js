f1dff9f17171d4e44296a474b794c6a686179295
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-12-05 14:44:00 +0200

    add input for list

---
+++ src/components/InputTodo.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class InputTodo extends Component {
+  state = {
+    title: '',
+  };
+  onChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.title.trim()) {
+      this.props.addTodoProps(this.state.title);
+      this.setState({
+        title: '',
+      });
+    } else {
+      alert('Please write item');
+    }
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          onChange={this.onChange}
+          placeholder="Add Todo..."
+          value={this.state.title}
+        />
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
+export default InputTodo;
